ad9ba6d8d8b4ec0bb78c07c34d286a0ae4da1eb0
Kwonharu <byhank0@naver.com>
2023-10-05 09:48:11 +0900

    a

--- src/main/java/com/javaex/controller/BlogController.java
+++ src/main/java/com/javaex/controller/BlogController.java
@@ -16,20 +16,31 @@ public class BlogController {
 	@Autowired
 	private BlogService blogService;
 	
+	//블로그 기본
 	@RequestMapping(value="/{id}", method={RequestMethod.GET, RequestMethod.POST})
 	public String blog(@PathVariable(value="id") String id, Model model){
 		System.out.println("BlogController.blog()");
+		System.out.println(id);
 		
 		UserVo userVo = blogService.getOneUser(id);
-		
+
 		if(userVo == null) {
 			return "error/403";
 		}else {
 			model.addAttribute(userVo);
 			return "blog/blog-main";
 		}
-
 	}
+	
+	//블로그 기본설정 수정
+	@RequestMapping(value="/{id}/admin/basic", method={RequestMethod.GET, RequestMethod.POST})
+	public String basic(@PathVariable(value="id") String id, Model model){
+		System.out.println("BlogController.basic()");
+		
+		
+		return "blog/admin/blog-admin-basic";
+	}
+	
 	
 }
 
